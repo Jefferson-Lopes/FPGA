052834a31b70eb5c35cf3428d218e762eb73e9ac
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-03 10:01:38 -0300

    done autocorrelation

--- correlator_C/autocorrelation.cpp
+++ correlator_C/autocorrelation.cpp
@@ -1,8 +1,9 @@
 /*
 Autocorrelação de sinais
 
-Feito só a estrutura inicial
-ainda falta implementar a lógica para fazer a correlação
+Programa para calcular a autocorrelação entre o mesmo sinal
+deslocado no tempo.
+
 
 by Jefferson Lopes
 */
@@ -12,29 +13,75 @@ by Jefferson Lopes
 
 using namespace std;
 
-#define SIZE 3 //quantidade de bits à ser analisado
+#define SIZE 5 //quantidade de bits à ser analisado
 
 void autocorrelation(int *, int *);
+void printCorrelation(int *, int *);
 
 int main(){
 	setlocale(LC_ALL, "");
 
 	int signal[SIZE] = {};
-	int correlation[2*SIZE - 1] = {};
-	
+	int correlation[(2*SIZE - 1)] = {}; 
 
-	cout << "Insira 3 números naturais para representar o sinal" << endl;
-	for (int i = 0; i < SIZE; i++){ cin  >> signal[i]; }
+	cout << "Insira " << SIZE << " numeros naturais para representar o sinal" << endl;
+	for (int i = 0; i < SIZE; i++) { cin  >> signal[i]; } 
 
-	for (int i = 0; i < SIZE; i++){ cout << signal[i]; }
+	autocorrelation(signal, correlation);
 
+	printCorrelation(signal, correlation);
 
 	return 0;
 }
 
 void autocorrelation(int *signal, int *correlation){
-	for(int i = 0, j = 0, y = 0; i < (2*SIZE - 1); i++){
-
+	for(int i = 0, hold = 0, diffe = 0; i < (2*SIZE - 1); i++, hold = 0){
+		if(i < SIZE){
+			diffe = (SIZE - 1) - i;
+			for(int y = 0; y <= i; y++, diffe++)
+				hold += signal[y] * signal[diffe];
+		}
+		else
+			hold = correlation[(2*SIZE - 2) - i];
+		
+		correlation[i] = hold;
 	}
-	//
+}
+
+void printCorrelation(int *signal, int *correlation){
+	cout << endl << endl;
+	for(int i = 0; i < (2*SIZE - 1); i++){
+		if(i < SIZE){
+			for(int y = 0; y < (SIZE - 1 - i); y++)
+				cout << "  ";
+
+			for(int y = 0; y < SIZE; y++)
+				cout << signal[y] << " ";
+
+			cout << endl;
+
+			for(int y = 0; y < SIZE; y++)
+				cout << signal[y] << " ";
+
+			cout << endl;
+			cout << "Autocorrelation " << correlation[i];
+			cout << endl << endl;
+		}
+		else{
+			for(int y = 0; y < SIZE; y++)
+				cout << signal[y] << " ";
+
+			cout << endl;
+
+			for(int y = 0; y <= (i - SIZE); y++)
+				cout << "  ";
+
+			for(int y = 0; y < SIZE; y++)
+				cout << signal[y] << " ";
+
+			cout << endl;
+			cout << "Autocorrelation " << correlation[i];
+			cout << endl << endl;
+		}
+	}
 }
