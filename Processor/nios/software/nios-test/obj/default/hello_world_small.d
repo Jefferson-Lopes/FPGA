1d37861fb7569b0c3ef5cbbf3636d36986e58626
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-08-03 18:08:13 -0300

    Add C code

---
+++ Processor/nios/software/nios-test/obj/default/hello_world_small.d
@@ -0,0 +1,4 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ C:/Users/jeff7/Documents/GitHub/FPGA/Processor/nios/software/nios-test_bsp/HAL/inc/sys/alt_stdio.h
+
+C:/Users/jeff7/Documents/GitHub/FPGA/Processor/nios/software/nios-test_bsp/HAL/inc/sys/alt_stdio.h:
